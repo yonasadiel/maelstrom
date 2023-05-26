f8967f3e1c47f5033cdd28ac6e6e7b97cc28d243
Yonas Adiel Wiguna <yonazadielwiguna@gmail.com>
2023-05-26 18:10:10 +0800

    Echo: proper structure

--- 1-echo/main.go
+++ 1-echo/main.go
@@ -7,22 +7,45 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-func main() {
-	n := maelstrom.NewNode()
+type EchoReq struct {
+	Type  string `json:"type"`
+	MsgId int64  `json:"msg_id"`
+	Echo  string `json:"echo"`
+}
 
-	n.Handle("echo", func(msg maelstrom.Message) error {
-		// Unmarshal the message body as an loosely-typed map.
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
+type EchoResp struct {
+	Type  string `json:"type"`
+	MsgId int64  `json:"msg_id"`
+	Echo  string `json:"echo"`
+}
+
+func Echo(msg maelstrom.Message) (any, error) {
+	reqBody := EchoReq{}
+	if err := json.Unmarshal(msg.Body, &reqBody); err != nil {
+		return nil, err
+	}
+
+	respBody := EchoResp{
+		Type:  "echo_ok",
+		MsgId: reqBody.MsgId,
+		Echo:  reqBody.Echo,
+	}
+	return respBody, nil
+}
+
+func wrapHandler(n *maelstrom.Node, f func(msg maelstrom.Message) (any, error)) func(msg maelstrom.Message) error {
+	return func(msg maelstrom.Message) error {
+		resp, err := f(msg)
+		if err != nil {
 			return err
 		}
+		return n.Reply(msg, resp)
+	}
+}
 
-		// Update the message type to return back.
-		body["type"] = "echo_ok"
-
-		// Echo the original message back with the updated message type.
-		return n.Reply(msg, body)
-	})
+func main() {
+	n := maelstrom.NewNode()
+	n.Handle("echo", wrapHandler(n, Echo))
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
